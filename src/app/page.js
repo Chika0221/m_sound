e1fd8270b8bd491b159b31fe3be17a4be6568550
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-02-14 16:48:57 +0900

    オーディオファイルを追加し、クリックイベントで音を再生する機能を実装

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,61 @@
-import Image from "next/image";
+"use client"
+import { useEffect } from 'react';
 
-export default function Home() {
+export default function Home(){
+
+  useEffect(() => {
+    const audio1 = new Audio("/audios/dokan.wav");
+    const audio2 = new Audio("/audios/coin.wav");
+    const audio3 = new Audio("/audios/1up.wav");
+    const audio4 = new Audio("/audios/suichu.wav");
+    const audio5 = new Audio("/audios/goal_flag.wav");
+    const audio6 = new Audio("/audios/select_course.wav");
+    const audios = [audio1, audio2, audio3, audio4, audio5, audio6];
+
+    function playSound(index){
+      audios[index].play()
+    }
+
+    const containers = document.querySelectorAll('.container');
+    containers.forEach((container, index) => {
+      container.addEventListener("click", () => playSound(index));
+    });
+
+    
+
+    return () => {
+      containers.forEach((container, index) => {
+        container.removeEventListener('click', playSound(index));
+      });
+    };
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="main">
+      <div className="container">
+        <p>土管</p>
+      </div>
+      
+      <div className="container">
+        <p>コイン</p>
+      </div>
+      
+      <div className="container">
+        <p>1UP</p>
+      </div>
+      
+      <div className="container">
+        <p>泳ぎ</p>
+      </div>
+      
+      <div className="container">
+        <p>ゴール</p>
+      </div>
+      
+      <div className="container">
+        <p>3ステージセレクト</p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+  )
 }
